8135e5ef11ad8b912e7c8970fcf805f81591d4a4
読み上げ <67372333+readloud@users.noreply.github.com>
2022-09-26 15:22:35 +0700

    Add files via upload

---
+++ cx_Freeze/samples/bcrypt/setup.py
@@ -0,0 +1,29 @@
+"""A setup script to demonstrate build using bcrypt.
+
+Run the build process by running the command 'python setup.py build'
+
+If everything works well you should find a subdirectory in the build
+subdirectory that contains the files needed to run the script without Python"""
+
+from cx_Freeze import Executable, setup
+
+executables = [Executable("test/__init__.py", target_name="test_bcrypt")]
+options = {
+    "build_exe": {
+        "excludes": ["tkinter"],
+        "zip_include_packages": ["*"],
+        "zip_exclude_packages": [],
+    }
+}
+
+setup(
+    name="test_bcrypt",
+    version="0.3",
+    description="cx_Freeze script to test bcrypt",
+    executables=executables,
+    options=options,
+    packages=["test"],
+    author="Marcelo Duarte",
+    author_email="marcelotduarte@users.noreply.github.com",
+    url="https://github.com/marcelotduarte/cx_Freeze/",
+)
---
+++ cx_Freeze/samples/build_constants/hello.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import sys
+from datetime import datetime
+
+import BUILD_CONSTANTS
+
+print("Hello from cx_Freeze")
+print(
+    "The current date is %s\n"
+    % datetime.today().strftime("%B %d, %Y %H:%M:%S")
+)
+
+print(f"Executable: {sys.executable!r}\n")
+
+
+excludedVars = [
+    "__builtins__",
+    "__cached__",
+    "__doc__",
+    "__loader__",
+    "__package__",
+    "__spec__",
+]
+
+print("== variables in BUILD_CONSTANTS ==\n")
+for var in dir(BUILD_CONSTANTS):
+    if var in excludedVars:
+        continue
+    attr = BUILD_CONSTANTS.__getattribute__(var)
+    print(f"{var} = {attr!r}")
