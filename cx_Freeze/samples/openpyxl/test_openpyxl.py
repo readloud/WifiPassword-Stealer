67e3957e7ec56e11c3170952a1b110bc2db15d53
読み上げ <67372333+readloud@users.noreply.github.com>
2022-09-26 15:23:53 +0700

    Add files via upload

---
+++ cx_Freeze/samples/openpyxl/test_openpyxl.py
@@ -0,0 +1,25 @@
+"""code sample found in the openpyxl documentation"""
+# https://openpyxl.readthedocs.io/en/default.
+
+import datetime
+
+from openpyxl import Workbook
+
+wb = Workbook()
+
+# grab the active worksheet
+ws = wb.active
+
+# Data can be assigned directly to cells
+ws["A1"] = 42
+
+# Rows can also be appended
+ws.append([1, 2, 3])
+
+# Python types will automatically be converted
+ws["A2"] = datetime.datetime.now()
+
+# Save the file
+fileName = "sample.xlsx"
+wb.save(fileName)
+print("Wrote file", fileName)
